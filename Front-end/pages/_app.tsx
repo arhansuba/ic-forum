8403a7721c76c5928bb9137a006d56071832a130
arhansuba <subasiarhan3@gmail.com>
2024-08-01 22:46:46 +0300

    first commit

---
+++ Front-end/pages/_app.tsx
@@ -0,0 +1,54 @@
+import '@fontsource/poppins';
+import theme from '../theme';
+import type { AppProps } from 'next/app';
+import Navbar from '../components/Navbar';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Toaster, toast } from 'react-hot-toast';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { QueryClient, QueryClientProvider, QueryCache } from 'react-query';
+import { WagmiConfig, createClient } from 'wagmi';
+import { providers } from 'ethers';
+
+const localhostProvider = new providers.JsonRpcProvider('http://localhost:8545', {
+  name: 'dev',
+  chainId: 1337,
+  ensAddress: undefined,
+});
+// Give wagmi our provider config and allow it to autoconnect wallet
+const client = createClient({
+  autoConnect: true,
+  provider: localhostProvider,
+});
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+  queryCache: new QueryCache({
+    onError: () => {
+      toast.error(
+        'Network Error: Ensure Metamask is connected & on the same network that your contract is deployed to.'
+      );
+    },
+  }),
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    // Provide the WagmiConfig at the top-level of our app
+    <WagmiConfig client={client}>
+      <ChakraProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <Navbar />
+          <Component {...pageProps} />
+          <Toaster position='bottom-right' />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </ChakraProvider>
+    </WagmiConfig>
+  );
+}
+
+export default MyApp;
